28f568fdd00d111b3df1a9e15dcb0df917f84963
Yurii Mazalov <yurii.mazalov@gmail.com>
2025-11-02 21:16:57 +0100

    chore(dom): cache core random game elements and controls

--- js/script.js
+++ js/script.js
@@ -42,3 +42,29 @@ overlay.addEventListener("click", close);
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") close();
 });
+
+const randomPlayer1 = document.querySelector(".random__player1");
+const randomPlayer2 = document.querySelector(".random__player2");
+
+const randomPlayer1Count = randomPlayer1.querySelector(
+  ".random__player1-count"
+);
+const randomPlayer2Count = randomPlayer2.querySelector(
+  ".random__player2-count"
+);
+
+const randomPlayer1Ready = randomPlayer1.querySelector(
+  ".random__player1-ready"
+);
+const randomPlayer2Ready = randomPlayer2.querySelector(
+  ".random__player2-ready"
+);
+
+const randomWinner = document.querySelector(".random__winner");
+const randomWinnerName = document.querySelector(".random__winner-name");
+
+const randomStreak = document.querySelector(".random__streak");
+const randomHistory = document.querySelector(".random__history");
+
+const randomButtonStart = document.querySelector(".random__button-start");
+const randomButtonPlay = document.querySelector(".random__button-play");
