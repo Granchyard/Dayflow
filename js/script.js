dcdbb95ec4ec1e4759bced647aadef5cb958ed86
Yurii Mazalov <yurii.mazalov@gmail.com>
2025-11-02 21:15:11 +0100

    fix(input/mobile): prevent multi-touch zoom, gestures and dblclick zoom

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 "use strict";
 
+document.addEventListener(
+  "touchmove",
+  function (e) {
+    if (e.touches.length > 1) {
+      e.preventDefault();
+    }
+  },
+  { passive: false }
+);
+
+["gesturestart", "gesturechange", "gestureend"].forEach((type) =>
+  document.addEventListener(type, (e) => e.preventDefault(), { passive: false })
+);
+
+document.addEventListener(
+  "dblclick",
+  (e) => {
+    e.preventDefault();
+  },
+  { passive: false }
+);
+
 //sidebar
 
 const body = document.body;
@@ -9,11 +31,11 @@ const sidebar = document.querySelector(".sidebar");
 
 const close = () => {
   body.classList.remove("sidebar-open");
-}
+};
 
 const toggle = () => {
   body.classList.toggle("sidebar-open");
-}
+};
 
 burgerButton.addEventListener("click", toggle);
 overlay.addEventListener("click", close);
